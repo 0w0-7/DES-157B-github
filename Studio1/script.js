d2ca414aeca1a40a0e1f556569559ee4f574c30f
Alex Jr Chau <achau@ucdavis.edu>
2024-04-18 02:00:30 -0700

    studio 1 update

--- Studio1/script.js
+++ Studio1/script.js
@@ -1,21 +1,21 @@
 (function(){
     'use strict';
 
+    const body = document.querySelector('body');
+    const myVideo = document.querySelector('#myVideo');
+    const onClick = document.querySelector('#btn');
+    const fs = document.querySelector('.fa-expand');
+    
+    const source = document.querySelector('source');
+    const videos = ['media/scene1.mp4', 'media/scene2.mp4', 'media/scene3.mp4'];
+    let index = 0;
+
     const line1 = document.querySelector ('#line1');
     const line2 = document.querySelector ('#line2');
     const line3 = document.querySelector ('#line3');
     const line4 = document.querySelector ('#line4');
     const line5 = document.querySelector ('#line5');
     const line6 = document.querySelector ('#line6');
-
-    const myVideo = document.querySelector('#myVideo');
-
-    const intervalID = setInterval(checkTime, 1000);
-
-
-    const source = document.querySelector('source');
-    const videos = ['media/scene2.mp4', 'media/scene3.mp4', 'media/scene1.mp4'];
-    let index = 0;
 
 
     // if vid 1 is playing and time is between 0 and 5 display line 1
@@ -25,60 +25,100 @@
     // Style text to the left
     // Don't hide text until next change in video
 
-    function checkTime() {
-        console.log(myVideo.currentTime);
-        if (0 < myVideo.currentTime && myVideo.currentTime < 5) {
-            line1.className = "showing";
-        } else {
-            // line1.className = "hidden";
-        }
 
-        if (6 < myVideo.currentTime && myVideo.currentTime < 10) {
-            line2.className = "showing";
-        } else {
-            // line2.className = "hidden";
-        }
+    // myVideo.addEventListener('playing', function() {
+    //     loading.style.display = 'none';
+    // });
 
-        if (11 < myVideo.currentTime && myVideo.currentTime < 15) {
-            line3.className = "showing";
-        } else {
-            line3.className = "hidden";
+    onClick.addEventListener('click', myFunction);
+    function myFunction() {
+        if (index >= 0 && index <=2) {
+            index++;
+            
+            if (index == 3) {
+                index = 0;
+            }
         }
+            console.log(index);
 
-        if (16 < myVideo.currentTime && myVideo.currentTime < 20) {
-            line4.className = "showing";
-        } else {
-            line4.className = "hidden";
-        }
-        
-        if (11 < myVideo.currentTime && myVideo.currentTime < 15) {
-            line5.className = "showing";
-        } else {
-            line5.className = "hidden";
-        }
-
-        if (11 < myVideo.currentTime && myVideo.currentTime < 15) {
-            line6.className = "showing";
-        } else {
-            line6.className = "hidden";
-        }
+        source.setAttribute('src', videos[index]);
+        myVideo.load();
+        myVideo.play();
     }
-
 
     myVideo.addEventListener('ended', changeVideo);
 
     function changeVideo() {
         console.log('video ended');
 
+        // Increment first to avoid using old index
+        if (index >= videos.length - 1) {
+                    index = 0;
+                } else {
+                    index++;
+                }
+        
         source.setAttribute('src', videos[index]);
         myVideo.load();
         myVideo.play();
-        
-        if (index >= videos.length - 1) {
-            index = 0;
+    }
+
+
+    const poem = {
+        start: [0, 5, 8],
+        stop: [4, 7, 10],
+        line: [line1, line2, line3]
+    }
+    const intervalID = setInterval(checkTime, 1000);
+
+
+    function checkTime() {
+        console.log(myVideo.currentTime);
+        if (0 < myVideo.currentTime && myVideo.currentTime < 2) {
+            line1.className = "showing";
         } else {
-            index++;
+            line1.className = "hidden";
+        }
+
+        if (3 < myVideo.currentTime && myVideo.currentTime < 4) {
+            line2.className = "showing";
+        } else {
+            line2.className = "hidden";
+        }
+
+        if (5 < myVideo.currentTime && myVideo.currentTime < 6) {
+            line3.className = "showing";
+        } else {
+            line3.className = "hidden";
+        }
+
+        if (7 < myVideo.currentTime && myVideo.currentTime < 8) {
+            line4.className = "showing";
+        } else {
+            line4.className = "hidden";
+        }
+        console.log(index);
+        if (9 < myVideo.currentTime && myVideo.currentTime < 10) {
+            line5.className = "showing";
+        } else {
+            line5.className = "hidden";
+        }
+
+        if (11 < myVideo.currentTime && myVideo.currentTime < 15 && index == 1) {
+            line6.className = "showing";
+        } else {
+            line6.className = "hidden";
         }
     }
+
+    fs.addEventListener('click', function() {
+        // The fullscreenElement attribute returns null if the element is in windowed mode
+        if (!document.fullscreenElement) {
+            // document.documentElement returns the Element that is a direct child of the document, the <html> element
+            document.documentElement.requestFullscreen();
+        } else {
+            document.exitFullscreen();
+        }
+    });
 
 })();
